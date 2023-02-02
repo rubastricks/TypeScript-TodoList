d24f53fead941465ca4d17aa2381817cbfb0f557
Alisson Rubas <85270792+rubastricks@users.noreply.github.com>
2023-02-02 13:31:24 -0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+import React,{FC,useState, ChangeEvent} from 'react';
+import './App.css';
+import TodoTask from './Component/TodoTask';
+import { ITask } from './interfaces';
+
+const App: FC = () => {
+const [task, setTask] = useState<string>("");
+const [deadline, setDeadline] = useState<number>(0);
+const [todoList, setTodoList] = useState<ITask[]>([]);
+
+const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+if(event.target.name === 'task'){
+  setTask(event.target.value)
+}else{
+  setDeadline(Number(event.target.value));
+}
+} 
+
+const addTask = (): void => {
+  const newTask = {taskName: task, deadline: deadline}
+  setTodoList([...todoList, newTask]);
+setTask("");
+setDeadline(0);
+}
+
+const completeTask = (taskNameToDelete: string):void => {
+  setTodoList(todoList.filter((task) => {
+    return task.taskName != taskNameToDelete
+  }))
+}
+
+  return (
+ <div className='App'>
+<div className='header'>
+  <div className='inputContainer'>
+  <input type='text' 
+  placeholder='Task...' 
+  onChange={handleChange}
+value={task}
+  name='task'
+  />
+  <input type='number'
+   placeholder='Deadline' 
+  onChange={handleChange}
+value={deadline}
+  name='deadline'
+  />
+  <button onClick={addTask}>Add Task</button>
+  </div>
+</div>
+  <div className='todo-list'>
+    {todoList.map((task: ITask, key: number) => {
+      return <TodoTask key={key} task={task} completeTask={completeTask} />
+    })}
+  </div>
+
+ </div> 
+  )
+}
+
+export default App;
